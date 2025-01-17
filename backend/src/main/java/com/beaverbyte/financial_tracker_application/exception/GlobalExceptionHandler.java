d326e1499f30d2c851af538b7dd5390bb6e8f1ba
Nearere <97075670+BeaverByte@users.noreply.github.com>
2025-01-17 12:30:07 -0600

    Reformatted GlobalExceptionHandler

--- backend/src/main/java/com/beaverbyte/financial_tracker_application/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/beaverbyte/financial_tracker_application/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.beaverbyte.financial_tracker_application.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,18 +12,18 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Handle validation errors from @Valid annotation
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        
-        // Iterate over all validation errors
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+	// Handle validation errors from @Valid annotation
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
 
-        return ResponseEntity.badRequest().body(errors);
-    }
+		// Iterate over all validation errors
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+
+		return ResponseEntity.badRequest().body(errors);
+	}
 }
