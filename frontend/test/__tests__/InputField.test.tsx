50878ddd1dd046450e56d9b62c2229ffa2d837b8
Nearere <97075670+BeaverByte@users.noreply.github.com>
2025-02-06 15:27:41 -0600

    Update with vitest

---
+++ frontend/test/__tests__/InputField.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { InputField } from "../../src/components/Form/Input";
+import "@testing-library/jest-dom";
+import { assert, expect, test } from "vitest";
+
+test("Has correct label", async () => {
+  // ARRANGE
+  render(<InputField name="username" value="" onChange={() => {}} />);
+
+  const label = screen.getByLabelText("Username:");
+  // ACT
+  // await userEvent.click(screen.getByLabelText("Username:"));
+  // await screen.findByRole("heading");
+
+  // ASSERT
+  expect(label).toHaveTextContent("hello there");
+  expect(screen.getByRole("button")).toBeDisabled();
+});
