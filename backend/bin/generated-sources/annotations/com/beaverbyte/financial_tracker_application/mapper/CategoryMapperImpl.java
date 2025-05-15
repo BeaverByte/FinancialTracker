a31385ae00ea4b6ad391a75f3954f2949de0c867
Nearere <97075670+BeaverByte@users.noreply.github.com>
2025-05-15 14:43:46 -0500

    Update CI workflow to build env vars for secret injection

--- backend/bin/generated-sources/annotations/com/beaverbyte/financial_tracker_application/mapper/CategoryMapperImpl.java
+++ backend/bin/generated-sources/annotations/com/beaverbyte/financial_tracker_application/mapper/CategoryMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-14T21:59:12-0500",
+    date = "2025-05-15T12:52:08-0500",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
